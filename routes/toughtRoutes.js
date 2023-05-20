11cb456853da3b15ce3f68166a82b90544c9314e
Saulo de Tarso <de_tarso7@hotmail.com>
2023-05-20 14:46:18 -0300

    Criando Middleware blindar rota

--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
 const router = express.Router()
+
+const checkAuth = require('../helpers/auth').checkAuth
 
 const ToughtController = require('../constollers/ToughtController')
 
-router.get('/dashboard', ToughtController.dashboard)
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showTough)
 
 module.exports = router
