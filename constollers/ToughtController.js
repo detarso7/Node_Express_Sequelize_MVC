38e3ee51479dae72549a01adc0da489a6d0409c6
Saulo de Tarso <de_tarso7@hotmail.com>
2023-05-20 15:05:42 -0300

    View criação de pensamentos

--- constollers/ToughtController.js
+++ constollers/ToughtController.js
@@ -9,4 +9,8 @@ module.exports = class ToughtController{
     static async dashboard (req, res){
         res.render('toughts/dashboard')
     }
+
+    static createTought(rreq, res){
+        res.render('toughts/create')
+    }
 }
--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -5,6 +5,7 @@ const checkAuth = require('../helpers/auth').checkAuth
 
 const ToughtController = require('../constollers/ToughtController')
 
+router.get('/add', checkAuth, ToughtController.createTought)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showTough)
 
